3f67052c4357c4c69a6f88fd3f037370563cc5ea
deniel-leal <66749838+deniel-leal@users.noreply.github.com>
2020-06-10 17:01:33 -0300

    Delete unknowCommand.js

--- unknowCommand.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (client,msg) =>{
-    var message = msg.content.split(" ");
-    message = message[0];
-    msg.reply(`Comando '${message}' não existe\nDigite '!ajuda' para obter a lista de comandos`);
-}
